1057ea903c8f263eaca8000e4769dfc124355a55
chenjiongwei <johntop321@163.com>
2024-11-19 13:33:14 +0800

    基于数据字典文档学习编写SQL脚本测试

--- test1.sql
+++ test1.sql
@@ -1,4 +1,11 @@
-select * FROM myuser
-select * FROM myuser
-select * FROM myuser
 
+
+SELECT 
+    fi.FeeItemName AS 款项名称,
+    SUM(ti.Amount) AS 本周发票金额
+FROM TaxInvoice ti
+INNER JOIN FeeItem fi ON ti.FeeItemGUID = fi.FeeItemGUID
+--WHERE ti.CreateTime >= DATEADD(wk, DATEDIFF(wk, 0, GETDATE()), 0)  -- 本周一
+--AND ti.CreateTime < DATEADD(wk, DATEDIFF(wk, 0, GETDATE()) + 1, 0) -- 下周一
+GROUP BY fi.FeeItemName
+ORDER BY SUM(ti.Amount) DESC
