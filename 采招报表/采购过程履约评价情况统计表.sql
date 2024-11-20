b8bcf36d126cd58a41e0b49fa72fb3026c5d3766
chenjiongwei <johntop321@163.com>
2024-11-20 20:29:15 +0800

    创建《采购过程履约评价情况统计表》的查询脚本

---
+++ 采招报表/采购过程履约评价情况统计表.sql
@@ -0,0 +1,63 @@
+ --新增《采购过程履约评价情况统计表》，输出 经办人，采购过程名称、项目名称、定标完成时间、履约评价阶段，完成本阶段时间。
+
+SELECT   
+       a.BUGUID,
+       ProjectNameList AS 项目名称,
+       Manager AS 经办人,
+       SolutionName AS 采购过程名称,
+       WinBidProviderGUIDlist,
+       nd.RealEndDate 定标完成时间,
+       pft.PgName AS 履约评估名称,
+       pft.StageName AS 履约评价阶段,
+       pft.RealEndDate 完成本阶段时间
+FROM data_wide_cg_CgSolution a
+    INNER JOIN
+    (
+        SELECT SolutionGUID,
+               SolutionStepName,
+               PlanEndDate,
+               RealEndDate
+        FROM data_wide_cg_CgSolutionNode
+        WHERE SolutionStepName = '定标'
+              AND RealEndDate IS NOT NULL
+    ) nd
+        ON a.CgSolutionGUID = nd.SolutionGUID
+    OUTER APPLY
+(
+    SELECT TOP 1
+           pf.PgName,
+           pf.ProviderGUID,
+           pf.StageName,
+           pf.RealEndDate
+    FROM data_wide_cg_PgPerformTacticAgr pf
+    WHERE CHARINDEX(CONVERT(VARCHAR(36), pf.ProviderGUID), a.WinBidProviderGUIDlist) >= 1
+    ORDER BY RealEndDate DESC
+) pft
+WHERE 1 = 1 AND  WinBidProviderGUIDlist IS NOT NULL  AND  a.BUGUID IN (@var_buguid)
+
+/*
+-- 宽表中增加中标供应商GUID列表
+SELECT 
+    CgSolutionGUID,
+    ProviderGUIDlist,-- 中标供应商GUID列表
+    COUNT(DISTINCT([ProviderRecordGUID])) AS [WinBidProviderCount],  -- 中标供应商数量
+    SUM(ISNULL([WinBidPrice], 0)) AS [WinBidAmount]
+FROM (
+    SELECT  
+        CgSolutionGUID,
+        ProviderRecordGUID,
+        WinBidPrice,
+        IsWinBid,
+        (SELECT STUFF(
+            (SELECT DISTINCT ',' + CONVERT(VARCHAR(50), a.ProviderGUID)
+            FROM cg_CgProcReturnBidProvider a 
+            WHERE a.CgSolutionGUID = prbp.CgSolutionGUID 
+                AND a.[IsWinBid] = 1
+            FOR XML PATH('')),
+            1, 1, ''
+        )) AS ProviderGUIDlist
+    FROM [dbo].[cg_CgProcReturnBidProvider] prbp
+) AS ProcReturnBidProvider 
+WHERE [IsWinBid] = 1
+GROUP BY CgSolutionGUID,ProviderGUIDlist
+*/
